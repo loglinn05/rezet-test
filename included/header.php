69662688444271aa5cb096103d608aedf5271226
loglinn05 <loglinn05@gmail.com>
2021-12-26 13:58:33 +0200

    Changed header background image

--- included/header.php
+++ included/header.php
@@ -22,7 +22,7 @@
                 <div class = "container">
                     <a class = "navbar-brand font-weight-bold" href="#" data-toggle = "modal" data-target="#modalPush">
                         <img src = "img/logo.png"  class = "d-inline-block align-center ">
-                        <strong style = "color:#000; text-shadow:2px 2px 3px #fff;">&nbsp; Test</strong>
+                        <strong style = "color:#fff; text-shadow: 2px 2px 3px #bbb;">&nbsp; Test</strong>
                     </a>
                     <button class = "navbar-toggler" type = "button" data-toggle = "collapse" data-target="#navbarSupportedContent-7" aria-controls = "navbarSupportedContent-7" aria-expanded = "false" aria-label = "Toggle navigation">
                     <span class = "navbar-toggler-icon"></span>
@@ -34,13 +34,13 @@
                     <div class = "container">
                         <div class = "row mt-5">
                             <div class = "col-md-12 mb-4 white-text text-center">
-                                <h1 class = "h1-reponsive white-text text-uppercase font-weight-bold mb-0 pt-md-5 pt-5 wow fadeInDown" data-wow-delay = "0.3s"><strong>T E S T</strong></h1>
+                                <h1 class = "h1-reponsive white-text text-uppercase font-weight-bold mb-0 pt-md-5 pt-5 wow fadeInDown" data-wow-delay = "0.3s" style = "text-shadow: 0px 0px 10px #000;"><strong>T E S T</strong></h1>
                                 <hr class = "hr-light my-4 wow fadeInDown" data-wow-delay = "0.4s">
-                                <h5 class = "text-uppercase mb-4 white-text wow fadeInDown" data-wow-delay = "0.4s">
+                                <h5 class = "text-uppercase mb-4 white-text wow fadeInDown" data-wow-delay = "0.4s" style = "text-shadow: 0px 0px 10px #000;">
                                     <strong>R E Z E T</strong>
                                 </h5>                                
-                                <a class = "btn btn-outline-white wow fadeInDown" href = "index.php" data-wow-delay = "0.4s">PHP code</a>
-                                <a class = "btn btn-outline-white wow fadeInDown" href = "js_code.php" data-wow-delay = "0.4s">JS code</a>
+                                <a class = "btn btn-outline-white wow fadeInDown" href = "index.php" data-wow-delay = "0.4s" style = "text-shadow: 0px 0px 10px #000;">PHP code</a>
+                                <a class = "btn btn-outline-white wow fadeInDown" href = "js_code.php" data-wow-delay = "0.4s" style = "text-shadow: 0px 0px 10px #000;">JS code</a>
                             </div>                            
                         </div>
                     </div>
